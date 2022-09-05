5976bfa2f29fb1ae65ccd13b0e2c949a501dc771
WouterB15 <w.barf@live.nl>
2022-09-05 13:59:16 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,21 +27,21 @@ function main() {
 
     function render(time) {
         time *= 0.001;  // convert time to seconds
-       
+
         // cube.rotation.x = time;
         // cube.rotation.y = time;
-       
+
         renderer.render(scene, camera);
-       
+
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
 }
 
 
-function createCube(size, pos, color){
+function createCube(size, pos, color) {
     const geometry = new THREE.BoxGeometry(size.x, size.y, size.z);
-    const material = new THREE.MeshPhongMaterial({color})
+    const material = new THREE.MeshPhongMaterial({ color })
     const cube = new THREE.Mesh(geometry, material);
 
     cube.position = pos;
