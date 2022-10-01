6313260ed594e740328f669f6dfee1ea56e7bd47
WouterB15 <w.barf@live.nl>
2022-10-01 15:08:54 +0200

    Update AI.js

--- js/AI.js
+++ js/AI.js
@@ -3,20 +3,44 @@ import { Graph } from "./Astar/Graph.js";
 import { Level } from "./Level.js";
 import * as THREE from "../node_modules/three/build/three.module.js";
 
+const THRESHOLD = 0.2;
+
 export class Ai {
 	//add general code for the AI (pathfinding etc)
 
-	pathfinding() {
-		let astar = new Astar();
-		let data = Level.getLevelData;
+	astar = new Astar();
+
+	getPath(pacmanPos, playerPos) {
+		// console.log(pacmanPos);
+
 		let graph = new Graph(Level.getLevelData);
-		//console.log(graph);
+		graph.diagonal = true;
 
-		var start = graph.grid[13][6];
-		var end = graph.grid[2][3];
-		//console.log(start + end)
-		// var result = astar.search(graph, start, end);
+		var start = graph.grid[pacmanPos.x][pacmanPos.z];
+		var end = graph.grid[playerPos.x][playerPos.z];
+		// console.log(graph.grid[0].length);
 
-		// console.log(result[0].x);
+		graph.diagonal = true;
+		graph.dontCrossCorners = true;
+		var result = this.astar.search(graph, start, end);
+
+		return result;
+	}
+
+	// not used atm
+	isPositionReached(pos, targetpos) {
+		let deltaPos = new THREE.Vector3(
+			pos.x - targetpos.x,
+			0,
+			pos.z - targetpos.z
+		);
+
+		if (deltaPos.x > THRESHOLD || deltaPos.x < -THRESHOLD) {
+			return false;
+		}
+		if (deltaPos.z > THRESHOLD || deltaPos.z < -THRESHOLD) {
+			return false;
+		}
+		return true;
 	}
 }
