6f22ba37bbc4e3707faa4b178e56a2465e528ff7
tnoorman <teshalenoorman@gmail.com>
2022-10-05 14:22:17 +0200

    Changed styling, refactored Menu.js

--- js/Menu.js
+++ js/Menu.js
@@ -1,13 +1,22 @@
 export class Menu {
 
     constructor() {
+        this._initMainMenu();
+    }
+
+    _initMainMenu() {
+        this.eyeAnimation();
+    }
+
+    _initHelpMenu() {}
+
+    eyeAnimation() {
         const eye = document.getElementById('pupil');
         window.addEventListener('mousemove', (event) => {
-            const x = -(window.innerWidth / 2 - event.pageX) / 10;
-            const y = -(window.innerHeight / 2 - event.pageY) / 10;
+            const x = -(window.innerWidth / 2 - event.pageX) / 30;
+            const y = -(window.innerHeight / 1.5 - event.pageY) / 7;
 
             eye.style.transform = `translateY(${y}px) translateX(${x}px)`;
-
         })
     }
 }
