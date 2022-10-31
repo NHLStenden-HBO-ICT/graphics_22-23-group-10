05ebcbda3c9e131a192cf5bf2ade4effbbfa209b
laure <laurensmoedt@gmail.com>
2022-10-31 10:49:01 +0100

    hud changes + menu styling

--- js/HUD.js
+++ js/HUD.js
@@ -1,10 +1,10 @@
 export class HUD{
-    staminaBar(finalValue = 0) {
+    staminaBar(finalValue = 100) {
         const iframe = document.getElementById('HUD');
         const progress = iframe.contentWindow.document.querySelector('.progress-done');
         let max = 100;
         progress.style.width = `${(finalValue / max) * 100}%`;
-        progress.innerText = `${Math.ceil((finalValue / max) * 100)}%`;
+        // progress.innerText = `${Math.ceil((finalValue / max) * 100)}%`;
         // console.log("finalValue", finalValue);
 
     }
